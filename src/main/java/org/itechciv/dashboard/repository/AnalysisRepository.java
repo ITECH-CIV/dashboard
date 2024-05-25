65ec46dfe1e8348312938ccfe5fa059ac80dba05
ericettien <wilfriedettien@gmail.com>
2024-05-25 17:14:15 +0000

    Implémentation des endpoints Partenaire par âge selon une categorie CDC

--- src/main/java/org/itechciv/dashboard/repository/AnalysisRepository.java
+++ src/main/java/org/itechciv/dashboard/repository/AnalysisRepository.java
@@ -6875,6 +6875,9 @@ List<Object[]> getPatientByCDCMaleForRegimenAndPartnerOne(@Param("regimenId") Lo
   List<Object[]> getTestAndPatientResultForOnePartnerAndRegion(@Param("year") int year, @Param("partnerId") Long partnerId);
   
 
+
+
+
 //STATISTIQUES DE PERFORMNCE POUR CHAQUE LABORATOIRE
 @Query(value =  " SELECT l.id, l.name,   " +
 " SUM(CASE WHEN a.converted_result >= 1000 THEN 1 ELSE 0 END) AS TOTAL_SUPERIEUR_OU_EGAL_1000, " +
@@ -7362,7 +7365,85 @@ List<Object[]> getPatientByCDCMaleAndPartnerOne(@Param("year") int year, @Param(
   " GROUP BY pat.gender,cdc_age_cat.label, part.name, EXTRACT(YEAR FROM a.drcpt)", nativeQuery = true) 
   List<Object[]> getTestByCDCFemaleAndPartnerOne(@Param("year") int year, @Param("partnerId") Long partnerId, @Param("sex") String sex);
 
-  
+//PARTENAIRE PAR AGE
+  //NOMBRE DE TESTS REALISES PAR SPECIMENS POUR UNE PERIODE ET SELON UNE CATEGORIE D'AGE
+@Query(value = " SELECT TO_CHAR(a.drcpt, 'Mon-YYYY') AS date, st.label,  " +
+" COUNT(t.id) AS total_tests, " +
+" SUM(CASE WHEN st.label = 'DBS' THEN 1 ELSE 0 END) AS TOTAL_DBS, " +
+" SUM(CASE WHEN st.label = 'PSC' THEN 1 ELSE 0 END) AS TOTAL_PSC, " +
+" SUM(CASE WHEN st.label = 'Tube EDTA - Violet' THEN 1 ELSE 0 END) AS TOTAL_EDTA_PLASMA, " +
+" SUM(1) AS total_tests " +
+" FROM dashboard.region r " +
+" INNER JOIN dashboard.district d ON r.id = d.region_id" +
+" INNER JOIN dashboard.site s ON d.id = s.district_id " +
+" INNER JOIN dashboard.patient p ON s.id = p.site_id" +
+" INNER JOIN dashboard.analysis a ON p.id = a.patient_id " +
+" INNER join dashboard.test t on t.id = a.test_id " +
+" INNER join dashboard.site_partner sp on s.id = sp.site_id " +
+" INNER join dashboard.partner pt on pt.id = sp.partner_id " +
+" INNER JOIN dashboard.regimen rg ON a.regimen_id = rg.id " +
+" INNER JOIN dashboard.vl_reason vr ON a.vl_reason_id = vr.id " +
+" INNER JOIN dashboard.sample_type st ON a.sample_type_id = st.id " +
+" INNER JOIN dashboard.age_category ac ON ac.id = a.age_cdc_id "  +
+" WHERE a.drcpt BETWEEN '2023-01-01' AND CURRENT_DATE " + 
+" AND a.cdc_id = :ageCategoryId " +
+" AND cdc_age_cat.type = 'CDC CI' " +
+" GROUP BY a.drcpt,TO_CHAR(a.drcpt, 'Mon-YYYY'), st.label " +
+" ORDER BY a.drcpt ; " ,nativeQuery = true)
+List<Object[]> getTestBySpecimen(@Param("ageCategoryId") Long ageCategoryId);
+
+
+//NOMBRE DE TESTS REALISES POUR UNE ANNEE EN COURS SELON UNE CATEGORIE D'AGE
+
+ @Query(value = "SELECT   " +
+ " SUM(CASE WHEN a.converted_result >= 1000 THEN 1 ELSE 0 END) AS TOTAL_SUPERIEUR_OU_EGAL_1000, " +
+ " SUM(CASE WHEN a.converted_result < 1000 THEN 1 ELSE 0 END ) AS TOTAL_INFERIEUR_A_1000, " +
+ " SUM(CASE WHEN a.converted_result = 0 THEN 1 ELSE 0 END) AS TOTAL_INDETECTABLE_LL, " +
+ " SUM(CASE WHEN a.converted_result = -1 THEN 1 ELSE 0 END) AS TOTAL_INVALIDE_XXXX, " +
+ " SUM(1) AS TOTAL_TEST, " +
+ " SUM(CASE WHEN a.converted_result >= 1000 THEN 1 WHEN a.converted_result < 1000 AND a.converted_result = 0 THEN 1 ELSE 0 END)AS TOTAL_VALIDE,  " +
+ " ROUND(SUM(CASE WHEN a.converted_result >= 1000 THEN 1 ELSE 0 END) * 100.0 / SUM(1), 2) AS POURCENTAGE_SUPERIEUR_OU_EGAL_A_1000, " +
+ " ROUND(SUM(CASE WHEN a.converted_result < 1000 THEN 1 ELSE 0 END) * 100.0 / SUM(1), 2) AS POURCENTAGE_INFERIEUR_A_1000, " +
+ " ROUND(SUM(CASE WHEN a.converted_result = 0 THEN 1 ELSE 0 END) * 100.0 / SUM(1), 2) AS POURCENTAGE_INDETECTABLE, " +
+ " ROUND(SUM(CASE WHEN a.converted_result = -1 THEN 1 ELSE 0 END) * 100.0 / SUM(1), 2) AS POURCENTAGE_INVALIDE " +
+ " FROM dashboard.region r " +
+ " INNER JOIN dashboard.district d ON r.id = d.region_id" +
+ " INNER JOIN dashboard.site s ON d.id = s.district_id " +
+ " INNER JOIN dashboard.patient p ON s.id = p.site_id" +
+ " INNER JOIN dashboard.analysis a ON p.id = a.patient_id " +
+ " INNER JOIN dashboard.test t on t.id = a.test_id " +
+ " INNER JOIN dashboard.site_partner sp on s.id = sp.site_id " +
+ " INNER JOIN dashboard.partner pt on pt.id = sp.partner_id " +
+ " WHERE a.cdc_id = :ageCategoryId AND " +
+ " cdc_age_cat.type = 'CDC CI' AND " +
+ " EXTRACT(YEAR FROM a.drcpt) = :year " +
+ " GROUP BY EXTRACT(YEAR FROM a.drcpt);", nativeQuery = true)
+List<Object[]> getTestByAgeCategoryCdc(@Param("year") int year, @Param("ageCategoryId") Long ageCategoryId);
+
+ //NOMBRE DE PATIENTS TESTES - MASCULIN ET SELON UNE CATEGORIE D'AGE
+ @Query(value =  " SELECT pat.gender, cdc_age_cat.label," +
+ " SUM(CASE WHEN a.converted_result >= 1000 THEN 1 ELSE 0 END) AS TOTAL_NON_SUPPRIME, " +
+ " SUM(CASE WHEN a.converted_result < 1000 AND a.converted_result > 0 THEN 1 ELSE 0 END ) AS TOTAL_SUPPRIME, " +
+ " ROUND(SUM(CASE WHEN a.converted_result = 0 THEN 1 WHEN a.converted_result < 1000 AND a.converted_result > 0 THEN 1 ELSE 0 END) * 100.0 / SUM(1), 2) AS POURCENTAGE_SUPPRIME, " +
+ " ROUND(SUM(CASE WHEN a.converted_result >= 1000 THEN 1 ELSE 0 END) * 100.0 / SUM(1), 2) AS POURCENTAGE_NON_SUPPRIME " +
+ " FROM  dashboard.analysis a " +
+ " LEFT JOIN dashboard.patient pat on pat.id = a.patient_id " +
+ " LEFT JOIN dashboard.site s on s.id = pat.site_id " +
+ " JOIN dashboard.district dis on dis.id = s.district_id " +
+ " JOIN dashboard.region reg on reg.id = dis.region_id " +
+ " LEFT JOIN dashboard.age_category cdc_age_cat on cdc_age_cat.id = a.age_cdc_id " +
+ " LEFT JOIN dashboard.site_partner site_part on site_part.site_id = s.id " +
+ " JOIN dashboard.partner part on part.id = site_part.partner_id " +
+ " JOIN dashboard.regimen rg on rg.id = a.regimen_id " +
+ " WHERE cdc_age_cat.type = 'CDC CI' AND " +   
+ " pat.gender = :sex AND " +
+ " cdc_age_cat.id = :ageCategoryId AND " +
+ " EXTRACT(YEAR FROM a.drcpt) = :year  " +
+ " GROUP BY pat.gender,cdc_age_cat.label, EXTRACT(YEAR FROM a.drcpt)", nativeQuery = true) 
+ List<Object[]> getPatientMaleByCdc(@Param("year") int year, @Param("ageCategoryId") Long ageCategoryId, @Param("sex") String sex);
+
+
+
 
 
 
